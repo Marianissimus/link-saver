25681384afc7959a9b9d69ddf7781068104d0171
Marian Vidoe <marianissimus@gmail.com>
2019-09-10 15:58:04 +0300

    installed Vue, started addForm component

---
+++ src/components/AppForm.vue
@@ -0,0 +1,43 @@
+<template>
+	<form>
+    <p>
+      <label for="url">Url:</label>
+		  <input type="url" name="url" id="url" v-model="link.url" size="50"/>
+		</p>
+    <p>
+      <label for="notes">Notes:</label>
+		  <textarea rows="4" cols="50" size="50" id="notes" name="notes" v-model="link.notes" />
+		</p>
+    <p>
+		  <button type="submit">Send</button>
+    </p>
+		{{ link }}
+	</form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      link: {
+      	url: '',
+      	notes: '',
+      	tags: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  display: inline-block;
+  width: 100px;
+  text-align: left;
+}
+
+input, textarea {
+  width: 200px;
+  vertical-align: top;
+}
+</style>
